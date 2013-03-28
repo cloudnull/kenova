a78494593ad6519dea14bfc725fd8ccfc731ef5b
cloudnull <kevin@bkintegration.com>
2013-03-28 11:23:11 -0500

    update to fix , no longer existent plugins via github. Also did some code clean up.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 # - version      : 1.6
 # - License      : GPLv3
 # - usage        : bash install.sh
-# - notes        : Requires Python, NovaClient; 
+# - notes        : Requires Python, NovaClient;
 # - notes        : Git is needed for installation.
 # - bash_version : >= 3.2.48(1)-release
 #### ========================================= ####
@@ -26,7 +26,8 @@ SUPERNOVAGIT="https://github.com/rackerhacker/supernova.git"
 # Temp Install Directories
 LEGACYNOVACLIENTDIR="$TEMPDIR/python-lnovaclient"
 OPENSTACKNOVACLIENTDIR="$TEMPDIR/python-novaclient"
-RAXNOVACLIENTDIR="$TEMPDIR/rackspace-novaclient"
+RAXNOVACLIENT="rackspace-novaclient"
+RAXNOVACLIENTDIR="$TEMPDIR/$RAXNOVACLIENT"
 SUPERNOVADIR="$TEMPDIR/supernova"
 KEYRINGNAME="keyring"
 
@@ -52,13 +53,13 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 if [ ! $(which python) ];then
-clear
+    clear
     echo -e "\nThis is a Python Wrapper for a Python application, which requires that you have Python installed.\nYou will need to install Python in order to proceed."
     exit 1
 fi
 
 if [ ! $(which git) ];then
-clear
+    clear
     echo -e "\nGit was not found on this Instance. You should install GIT to ensure compatibility,\nhowever, I can use fall back mode to retrieve the files but it is not guaranteed to work.\nWhile Fall Back mode may work, I would recommend that you install a GIT client on this system before continuing.\n"
     echo -e "\nPlease go to \"http://git-scm.com/downloads\" and install git on your system.\n"
     exit 1
@@ -85,7 +86,7 @@ fi
 if [ -d /opt/ ];then
     echo '/opt/' >> $TEMPDIR/$BADMODSDIR
 fi
-		
+
 if [ -d /usr/ ];then
     echo '/usr/' >> $TEMPDIR/$BADMODSDIR
 fi
@@ -110,10 +111,8 @@ for BadD in $(cat $TEMPDIR/$BADMODSDIR); do
         find $BadD -name '*rackspace_auth_openstack*' -exec rm -rf {} \;
         find $BadD -name '*rackspace_novaclient*' -exec rm -rf {} \;
         find $BadD -name '*supernova*' -exec rm -rf {} \;
-	done;
+    done;
 done; >> $TEMPDIR/$BADMODSREMOVED
-
-
 
 if [ -z $TEMPDIR/$BADMODSREMOVED ];then
     echo "Record of Purge can be found here : $TEMPDIR/$BADMODSREMOVED"
@@ -121,68 +120,66 @@ fi
 
 echo 'Cleanup Done'
 
-
 # Install Setup Tools if needed
 PYTHONVERSIONNUMBER=$(python -c 'import sys; print sys.version[:3]')
 SETUPTOOLSVERSION=$(curl -s http://pypi.python.org/pypi/setuptools | grep "http://pypi.python.org/packages/$PYTHONVERSIONNUMBER/" | sed -e 's/<a\ href\=//g' -e 's/\"//g' -e 's/#.*//g' -e 's/^[ ]*//g' | grep -v .exe)
 SETUPTOOLSNAME=$(echo $SETUPTOOLSVERSION | awk -F '/' '{print $8}')
 
 echo "Checking for Python Setup Tools"
-CHECKSETUPTOOLS=$(python -c "try:
+CHECKSETUPTOOLS=$(python -c "
+try:
     import setuptools
 except ImportError, e:
     print 'FAIL'
 ")
 
-if [ "$CHECKSETUPTOOLS" == "FAIL" ];then  
-	echo -e "\nSetup Tools was not found on your System, I am attempting to install the Python Module.\n"
-        if [ -z "$SETUPTOOLSVERSION" ];then
-            echo -e "Sorry though your version of Python has no Version of SetupTools Available.\nPlease Goto http://pypi.python.org/pypi/setuptools to see if\nthere is a version available."
-            exit 1
-        fi
-
-echo -e "\nI am getting the installation files for python-setuptools."
-	cd $TEMPDIR/
-        curl -O $SETUPTOOLSVERSION
-            bash $TEMPDIR/$SETUPTOOLSNAME >> $NOVALOG
+if [ "$CHECKSETUPTOOLS" == "FAIL" ];then
+    echo -e "\nSetup Tools was not found on your System, I am attempting to install the Python Module.\n"
+    if [ -z "$SETUPTOOLSVERSION" ];then
+        echo -e "Sorry though your version of Python has no Version of SetupTools Available.\nPlease Goto http://pypi.python.org/pypi/setuptools to see if\nthere is a version available."
+        exit 1
+    fi
+    echo -e "\nI am getting the installation files for python-setuptools."
+    cd $TEMPDIR/
+    curl -O $SETUPTOOLSVERSION
+    bash $TEMPDIR/$SETUPTOOLSNAME >> $NOVALOG
 fi
 
 
 # Removing old openstack nova directory
 cd $TEMPDIR
-if [ -d $OPENSTACKNOVACLIENTDIR ];then 
-	rm -rf $OPENSTACKNOVACLIENTDIR
+if [ -d $OPENSTACKNOVACLIENTDIR ];then
+    rm -rf $OPENSTACKNOVACLIENTDIR
 fi
 
 echo -e "\nI am installating python-novaclient."
 cd $TEMPDIR
-    git clone $NOVAVERSIONGIT
-	
+git clone $NOVAVERSIONGIT
+
 if [ -f $OPENSTACKNOVACLIENTDIR/tools/pip-requires ];then
-	echo -e "\nFixing pip so that it only installs the known working prettytable module.\npip-requires File Found.\n"
-	if [ $(grep -i prettytable $OPENSTACKNOVACLIENTDIR/tools/pip-requires) ];then
-		sed /^prettytable/d $OPENSTACKNOVACLIENTDIR/tools/pip-requires > $TEMPDIR/pip-requires.mod
-			rm $OPENSTACKNOVACLIENTDIR/tools/pip-requires
-				mv $TEMPDIR/pip-requires.mod $OPENSTACKNOVACLIENTDIR/tools/pip-requires
-					echo 'prettytable==0.5' >> $OPENSTACKNOVACLIENTDIR/tools/pip-requires
-	fi
+    echo -e "\nFixing pip so that it only installs the known working prettytable module.\npip-requires File Found.\n"
+    if [ $(grep -i prettytable $OPENSTACKNOVACLIENTDIR/tools/pip-requires) ];then
+        sed /^prettytable/d $OPENSTACKNOVACLIENTDIR/tools/pip-requires > $TEMPDIR/pip-requires.mod
+        rm $OPENSTACKNOVACLIENTDIR/tools/pip-requires
+        mv $TEMPDIR/pip-requires.mod $OPENSTACKNOVACLIENTDIR/tools/pip-requires
+        echo 'prettytable==0.5' >> $OPENSTACKNOVACLIENTDIR/tools/pip-requires
+    fi
 fi
 
 if grep prettytable $OPENSTACKNOVACLIENTDIR/setup.py;then
-		sed 's/prettytable/prettytable==0\.5/g' $OPENSTACKNOVACLIENTDIR/setup.py > $TEMPDIR/setup.py
-			rm $OPENSTACKNOVACLIENTDIR/setup.py
-				mv $TEMPDIR/setup.py $OPENSTACKNOVACLIENTDIR/setup.py
+    sed 's/prettytable/prettytable==0\.5/g' $OPENSTACKNOVACLIENTDIR/setup.py > $TEMPDIR/setup.py
+    rm $OPENSTACKNOVACLIENTDIR/setup.py
+    mv $TEMPDIR/setup.py $OPENSTACKNOVACLIENTDIR/setup.py
 fi
 
 # Installing Openstack Nova Client
 cd $OPENSTACKNOVACLIENTDIR
-	python setup.py install >> $NOVALOG
+python setup.py install >> $NOVALOG
 
 cd $TEMPDIR
-if [ -d $OPENSTACKNOVACLIENTDIR ];then 
-	rm -rf $OPENSTACKNOVACLIENTDIR
+if [ -d $OPENSTACKNOVACLIENTDIR ];then
+    rm -rf $OPENSTACKNOVACLIENTDIR
 fi
-
 
 # Removing old rackspace nova directory
 cd $TEMPDIR
@@ -190,44 +187,28 @@ if [ -d $RAXNOVACLIENTDIR ];then
     rm -rf $RAXNOVACLIENTDIR
 fi
 
-# Installing Rackspace Nova Client Extensions
-echo -e "\nI am installating Rackspace python-novaclient Extensions."
-cd $TEMPDIR
-    git clone $RAXNOVAVERSIONGIT
-
-cd $RAXNOVACLIENTDIR
-    python setup.py install >> $NOVALOG
-
-cd $TEMPDIR
-if [ -d $RAXNOVACLIENTDIR ];then
-        rm -rf $RAXNOVACLIENTDIR
-fi
-
-
 # Removing old Legacy nova directory
 cd $TEMPDIR
-if [ -d $LEGACYNOVACLIENTDIR ];then 
-	rm -rf $LEGACYNOVACLIENTDIR
+if [ -d $LEGACYNOVACLIENTDIR ];then
+    rm -rf $LEGACYNOVACLIENTDIR
 fi
 
-# Installing the Legacy Nova 
+# Installing the Legacy Nova
 echo -e "\nI am Installing the python-lnovaclient."
 cd $TEMPDIR
-    git clone $LNOVAVERSIONGIT
+git clone $LNOVAVERSIONGIT
 
 cd $LEGACYNOVACLIENTDIR
-	python setup.py install >> $NOVALOG
+python setup.py install >> $NOVALOG
 
 cd $TEMPDIR
-if [ -d $LEGACYNOVACLIENTDIR ];then 
-	rm -rf $LEGACYNOVACLIENTDIR
+if [ -d $LEGACYNOVACLIENTDIR ];then
+    rm -rf $LEGACYNOVACLIENTDIR
 fi
-
 
 # Installing keyring
 echo -e "\nI am Installing KeyRing."
 cd $TEMPDIR
-
 $EZINST $KEYRINGNAME >> $NOVALOG
 
 # Removing old Supernova directory
@@ -239,23 +220,23 @@ fi
 # Installing Supernova
 echo -e "\nI am Installing Supernova."
 cd $TEMPDIR
-    git clone $SUPERNOVAGIT
+git clone $SUPERNOVAGIT
 
 cd $SUPERNOVADIR
-    python setup.py install >> $NOVALOG
+python setup.py install >> $NOVALOG
 
 cd $TEMPDIR
 if [ -d $SUPERNOVADIR ];then
     rm -rf $SUPERNOVADIR
 fi
 
+# Install the Rackspace NovaClient from PIP
+$EZINST $RAXNOVACLIENT >> $NOVALOG
 
 # Installer for the kenova command and control script
 echo -e "\nInstalling the kenova script\n"
 cp -v $WHEREAMI/kenova.sh /usr/bin/kenova >> $NOVALOG
 chmod +x /usr/bin/kenova
-
-
 echo -e "\nThe Log for the installation has been written to :\n$NOVALOG\n"
 
 exit 0
--- kenova.sh
+++ kenova.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # - title        : kenova
-# - description  : This script will assist in the use of the Cloud Servers Python Script..
+# - description  : This script will assist in the use of the python-novaclient
 # - author       : Kevin Carter
 # - date         : 2012-06-05
-# - version      : 1.4    
+# - version      : 1.4
 # - License      : GPLv3
 # - usage        : bash kenova
-# - notes        : Requires Python, NovaClient; 
+# - notes        : Requires Python, NovaClient;
 # - notes        : While not needed for operation, Git is needed for installation.
 # - bash_version : >= 3.2.48(1)-release
 # ================================================================================
 
 # Defined admin API file and name
-APICREDFILE=".adminapi"
+APICREDFILE=".supernova"
 KEYNAME="kenova"
 
 # Where is the script located
@@ -55,35 +55,34 @@ clear
 fi
 
 REMEMBERINFO(){
-  API1=$(awk '{print $1}' $INFO)
-  API2=$(awk '{print $2}' $INFO)
-  API3=$(awk '{print $3}' $INFO)
+    API1=$(awk '{print $1}' $INFO)
+    API2=$(awk '{print $2}' $INFO)
+    API3=$(awk '{print $3}' $INFO)
 
-# This prints the USERNAME and the API-KEY that you specified earlier
+    # This prints the USERNAME and the API-KEY that you specified earlier
     echo ''
     echo "Remember You have set the USERNAME : $API1"
     echo "Remember You have set the API-Key  : $API2"
-if [ "$API3" ];then
-	echo "The System returned a User DDI of  : $API3"
-fi
+    if [ "$API3" ];then
+        echo "The System returned a User DDI of  : $API3"
+    fi
 }
 
 GODEFINED(){
-# Go is used to start the script one a Username and API-KEY have been set
-# This is a sanity check to make sure that you have set an account
-if [ -f $INFO ];then
-
-	REMEMBERINFO
-
-# If check fails this prints to let you know
-                else
-                        echo 'AND THEN???'
-                        echo "You have not set an account., and to use this you need to..."
-                        echo "Use the command [ $0 new ] and then set the USERNAME and API-KEY"
-                        echo "Example : $0 new <SOME USERNAME> <SOME API-KEY>"
-                        echo ''
-                        exit 1
-fi
+    # Go is used to start the script one a Username and API-KEY have been set
+    # This is a sanity check to make sure that you have set an account
+    if [ -f $INFO ];then
+        REMEMBERINFO
+    
+    # If check fails this prints to let you know
+    else
+        echo 'AND THEN???'
+        echo "You have not set an account., and to use this you need to..."
+        echo "Use the command [ $0 new ] and then set the USERNAME and API-KEY"
+        echo "Example : $0 new <SOME USERNAME> <SOME API-KEY>"
+        echo ''
+        exit 1
+    fi
 }
 
 CHECKFOROLDPROCESS(){
@@ -100,9 +99,10 @@ if [ -f $INFO.removal ];then
     echo "I hate left overs..."
     $RM $INFO.removal > /dev/null
 fi
+
 if [ -f $INFO ];then
-echo "Removing Account Info..."
-$RM $INFO > /dev/null
+    echo "Removing Account Info..."
+    $RM $INFO > /dev/null
 fi
 
 # This makes sure that the Process PIDs are killed
@@ -111,6 +111,7 @@ if [ "$KILLINFO" ]; then
     for KI in $KILLINFO; do kill -9 $KI > /dev/null; done
     sleep 1
 fi
+
 if [ "$KILLSLEEP" ]; then
     echo -e "killing sleepy processes\n"
     for KS in $KILLSLEEP; do kill -9 $KS > /dev/null; done
@@ -119,130 +120,107 @@ fi
 }
 
 HELPINFORMATION(){
-echo -e "\nUsage: $0 <EXPRESSION>"
-echo -e "
-Admin Functions :
-    admin-set -- Used for setting a Key Ring Password used with a setup Endpoint.
-    admin     -- Used to specify a Username and API Key
-                 |_ Select a region, which is spcified in \"$ADMINAPIFILE\"
-
-Rackspace Specific Functions :
-    new       -- Used to specify a Username and API Key
-    lus       -- Used to access Legacy US Cloud Servers
-    luk       -- Used to access Legacy UK Cloud Servers
-    ous       -- Used to access Open Cloud US Cloud Servers,
-                 |_ You have to specify a Region
-                 |_ Available Regions are : ord & dfw
-
-    ouk       -- Used to access Open Cloud UK Cloud Servers
-    clean     -- Removes all temp files for user interactions,
-                 This is also done automatically every 30 minutes
-\n"
+    echo -e "\nUsage: $0 <EXPRESSION>"
+    echo -e "
+    Admin Functions :
+        admin-set -- Used for setting a Key Ring Password used with a setup Endpoint.
+        admin     -- Used to specify a Username and API Key
+                     |_ Select a region, which is spcified in \"$ADMINAPIFILE\"
+    
+    Rackspace Specific Functions :
+        new       -- Used to specify a Username and API Key
+        lus       -- Used to access Legacy US Cloud Servers
+        luk       -- Used to access Legacy UK Cloud Servers
+        ous       -- Used to access Open Cloud US Cloud Servers,
+                     |_ You have to specify a Region
+                     |_ Available Regions are : ord & dfw
+    
+        ouk       -- Used to access Open Cloud UK Cloud Servers
+        clean     -- Removes all temp files for user interactions,
+                     This is also done automatically every 30 minutes
+    \n"
 }
-
-
 case "$1" in
 
 new)
 CHECKFOROLDPROCESS
-
 # This is a sanity check to make sure you have specified a USERNAME
 if [ -z $2 ];then
-echo "You have not specified a USERNAME, Please try again"
-exit 1
-
-# This is a Sanity check to make sure you have specified a API-KEY 
-# This also makes sure that the API-KEY is at least 20 characters long 
-	elif [ ! $(echo ${3} | wc -c) -gt 20 ];then 
-		echo "Your API-KEY is not long enough or you did not put one, Please try again..."
-		exit 1
-		else
-		
-# This is the translation from seconds to minutes
-TTLFORINFO=$( expr $SLEEPTIME / 60 )
-
-# Bulding API Cookie 
-echo "$2 $3" > $INFO
-	DDI=`kenova ous credentials | awk -F "'" '/tenant/ {print $4}'`
-		echo "$2 $3 $DDI" > $INFO
-            chmod 600 $INFO
-
-# Here a notice of what was entered is shown and the username and API-KEY tmp file is created 
-			echo -e "\n\n\nThis will allow you to control the Cloud servers from the Command Line..."
-			echo "HAL has saved the declarations to a TMP file located at $INFO"
-			echo -e "The information that you have entered will be saved for $TTLFORINFO Minutes\n"
-			echo "You have specified the USERNAME to be : $2"
-			echo "You have specified the API KEY  to be : $3"
-		if [ "$DDI" ];then 
-			echo "The System returned a User DDI of     : $DDI"
-		fi
-			echo "now use the command [ $0 <FUNCTION> ] to control your servers"
-			echo -e "Use [ $0 help ] for a full list of Functions\n"
-
-# The time out script is created in TMP and then loaded as a background process 
-echo -e "#!/bin/bash\nsleep $SLEEPTIME\n$RM $INFO\n$RM $INFO.removal\nexit 0" > $INFO.removal
-	chmod +x $INFO.removal
-		$INFO.removal &
-			exit 0
+    echo "You have not specified a USERNAME, Please try again"
+    exit 1
+    # This is a Sanity check to make sure you have specified a API-KEY
+    # This also makes sure that the API-KEY is at least 20 characters long
+elif [ ! $(echo ${3} | wc -c) -gt 20 ];then
+    echo "Your API-KEY is not long enough or you did not put one, Please try again..."
+    exit 1
+else
+    # This is the translation from seconds to minutes
+    TTLFORINFO=$( expr $SLEEPTIME / 60 )
+    # Bulding API Cookie
+    echo "$2 $3" > $INFO
+    DDI=`kenova ous credentials | awk -F "'" '/tenant/ {print $4}'`
+    echo "$2 $3 $DDI" > $INFO
+    chmod 600 $INFO
+    # Here a notice of what was entered is shown and the username and API-KEY tmp file is created
+    echo -e "\n\n\nThis will allow you to control the Cloud servers from the Command Line..."
+    echo "HAL has saved the declarations to a TMP file located at $INFO"
+    echo -e "The information that you have entered will be saved for $TTLFORINFO Minutes\n"
+    echo "You have specified the USERNAME to be : $2"
+    echo "You have specified the API KEY  to be : $3"
+    if [ "$DDI" ];then
+        echo "The System returned a User DDI of     : $DDI"
+    fi
+    echo "now use the command [ $0 <FUNCTION> ] to control your servers"
+    echo -e "Use [ $0 help ] for a full list of Functions\n"
+    # The time out script is created in TMP and then loaded as a background process
+    echo -e "#!/bin/bash\nsleep $SLEEPTIME\n$RM $INFO\n$RM $INFO.removal\nexit 0" > $INFO.removal
+    chmod +x $INFO.removal
+    $INFO.removal &
+    exit 0
 fi
 ;;
-
 clean)
 CHECKFOROLDPROCESS
-    exit 1
+exit 1
 ;;
-
 help)
 # This prints the help section of the script on your screen
 HELPINFORMATION
 ;;
- 
 lus)
-
 GODEFINED
-
-# This is a sanity check to make sure you have listed a function 
-   if [ ! -z $2 ];then
-        $LNOVA --url $V1USIDENTITY --username $API1 --apikey $API2 $2 $3 $4 $5 $6 $7 $8 $9;
-
-# if no function was listed go will let you know and then show the help screen 
-                else
-                        echo ''
-			echo "AND THEN??? You did not give any arguments, try again..."
-			echo ''
-                        $LNOVA help
-                        exit 0
-
-   fi
-;;
-
-luk)
-
-GODEFINED
-
-# This is a sanity check to make sure you have listed a function 
-   if [ ! -z $2 ];then
-        $LNOVA --url $V1UKIDENTITY --username $API1 --apikey $API2 $2 $3 $4 $5 $6 $7 $8 $9;
-
-# if no function was listed go will let you know and then show the help screen 
-                else
-                        echo ''
-			echo "AND THEN??? You did not give any arguments, try again..."
-			echo ''
-                        $LNOVA help
-                        exit 0
-
-   fi
-;;
-
-ous)
-
-GODEFINED
-
-if [ -z "$API3" ];then
-	API3="self"
+# This is a sanity check to make sure you have listed a function
+if [ ! -z $2 ];then
+    $LNOVA --url $V1USIDENTITY --username $API1 --apikey $API2 $2 $3 $4 $5 $6 $7 $8 $9;
+    # if no function was listed go will let you know and then show the help screen
+else
+    echo ''
+    echo "AND THEN??? You did not give any arguments, try again..."
+    echo ''
+    $LNOVA help
+    exit 0
 fi
+;;
+luk)
+GODEFINED
+# This is a sanity check to make sure you have listed a function
+if [ ! -z $2 ];then
+    $LNOVA --url $V1UKIDENTITY --username $API1 --apikey $API2 $2 $3 $4 $5 $6 $7 $8 $9;
+    # if no function was listed go will let you know and then show the help screen
+else
+    echo ''
+    echo "AND THEN??? You did not give any arguments, try again..."
+    echo ''
+    $LNOVA help
+    exit 0
 
+fi
+;;
+ous)
+GODEFINED
+if [ -z "$API3" ];then
+        API3="self"
+fi
 OS_USERNAME="${API1}"
 OS_PASSWORD="${API2}"
 OS_TENANT_NAME="${API3}"
@@ -250,44 +228,35 @@ NOVA_RAX_AUTH=1
 OS_AUTH_URL="$V2USIDENTITY"
 NOVA_VERSION=2
 NOVA_SERVICE_NAME=cloudServersOpenStack
-
 if [ -z "$2" ];then
-	echo "You need to specify a region, I can't proceed without the region."
-	exit 1
-		else 
-			if [ "$2" == "dfw" ] || [ "$2" == "DFW" ];then
-				OS_REGION_NAME="DFW"
-					elif [ "$2" == "ord" ] || [ "$2" == "ORD" ];then
-						OS_REGION_NAME="ORD"
-						else 
-							echo "The region was not specified or was invalid."
-							echo "Please try Again."
-							exit 1
-			fi		
-
-fi		
-
+    echo "You need to specify a region, I can't proceed without the region."
+    exit 1
+else
+    if [ "$2" == "dfw" ] || [ "$2" == "DFW" ];then
+        OS_REGION_NAME="DFW"
+    elif [ "$2" == "ord" ] || [ "$2" == "ORD" ];then
+        OS_REGION_NAME="ORD"
+    else
+        echo "The region was not specified or was invalid."
+        echo "Please try Again."
+        exit 1
+    fi
+fi
 export OS_USERNAME OS_REGION_NAME NOVA_RAX_AUTH OS_PASSWORD OS_AUTH_URL NOVA_VERSION NOVA_SERVICE_NAME OS_TENANT_NAME
-
-# This is a sanity check to make sure you have listed a function 
-   if [ ! -z $3 ];then
-        $NOVA $3 $4 $5 $6 $7 $8 $9;
-
-# if no function was listed go will let you know and then show the help screen 
-                else
-                        echo ''
-			echo "AND THEN??? You did not give any arguments, try again..."
-			echo ''
-                        $NOVA help
-                        exit 0
-
-   fi
+# This is a sanity check to make sure you have listed a function
+if [ ! -z $3 ];then
+    $NOVA $3 $4 $5 $6 $7 $8 $9;
+    # if no function was listed go will let you know and then show the help screen
+else
+    echo ''
+    echo "AND THEN??? You did not give any arguments, try again..."
+    echo ''
+    $NOVA help
+    exit 0
+fi
 ;;
-
 ouk)
-
 GODEFINED
-
 OS_USERNAME="${API1}"
 OS_PASSWORD="${API2}"
 OS_TENANT_NAME="LONUSER"
@@ -295,74 +264,61 @@ NOVA_RAX_AUTH=1
 OS_AUTH_URL="$V2UKIDENTITY"
 NOVA_VERSION=2
 NOVA_SERVICE_NAME=cloudServersOpenStack
-
-    OS_REGION_NAME="LON"
-
+OS_REGION_NAME="LON"
 export OS_USERNAME OS_REGION_NAME NOVA_RAX_AUTH OS_PASSWORD OS_AUTH_URL NOVA_VERSION NOVA_SERVICE_NAME OS_TENANT_NAME
-
-# If you use the go function it expects other functions too 
-# This is a sanity check to make sure you have listed a function 
-   if [ ! -z $2 ];then
-        $NOVA $2 $3 $4 $5 $6 $7 $8 $9;
-
-# if no function was listed go will let you know and then show the help screen 
-                else
-                        echo ''
-			echo "AND THEN??? You did not give any arguments, try again..."
-			echo ''
-                        $NOVA help
-                        exit 0
-
-   fi
+# If you use the go function it expects other functions too
+# This is a sanity check to make sure you have listed a function
+if [ ! -z $2 ];then
+     $NOVA $2 $3 $4 $5 $6 $7 $8 $9;
+    # if no function was listed go will let you know and then show the help screen
+else
+    echo ''
+    echo "AND THEN??? You did not give any arguments, try again..."
+    echo ''
+    $NOVA help
+    exit 0
+fi
 ;;
-
 admin)
-
 # source Information provided by $ADMINAPIFILE
-
 if [ -z "$2" ];then
     echo "You need to specify a region, I can't proceed without the region."
     exit 1
-        else
-            if [ -f "$ADMINAPIFILE" ];then
-                ADMINENDPOINT=$(grep "\[$2\]" $ADMINAPIFILE)
-                    if [ "$ADMINENDPOINT" ];then
-                        PARSEDFILE=$(sed -n -e "/\[$2\]/,/^$/p" $ADMINAPIFILE | sed -e '/^\[/d' -e 's/\ //g' -e '')
-                            if [ "$(echo \"$PARSEDFILE\" | grep 'USE_KEYRING')" ];then
-                                PASSKEY=$(echo -e "$PARSEDFILE" | awk -F '=' '/OS_PASSWORD/ || /NOVA_API_KEY/ {print $1}')
-                                PASSWORD=$(python -c "import keyring;print keyring.get_password('""$KEYNAME""', '""$2:$PASSKEY""')")
-                                KEYRINGPARSEDFILE=$(sed -n -e "/\[$2\]/,/^$/p" $ADMINAPIFILE | sed -e '/^\[/d' -e 's/\ //g' -e "s/USE_KEYRING/$PASSWORD/g")
-                                export $KEYRINGPARSEDFILE
-                                    else
-                                        export $PARSEDFILE
-                            fi
-                        else
-                            echo "The region was not specified or was invalid."
-                            echo "Please try Again."
-                            exit 1
-                    fi
-                else
-                    echo "Admin API config File is not found."
-                    exit 1
+else
+    if [ -f "$ADMINAPIFILE" ];then
+        ADMINENDPOINT=$(grep "\[$2\]" $ADMINAPIFILE)
+        if [ "$ADMINENDPOINT" ];then
+            PARSEDFILE=$(sed -n -e "/\[$2\]/,/^$/p" $ADMINAPIFILE | sed -e '/^\[/d' -e 's/\ //g' -e '')
+            if [ "$(echo \"$PARSEDFILE\" | grep 'USE_KEYRING')" ];then
+                PASSKEY=$(echo -e "$PARSEDFILE" | awk -F '=' '/OS_PASSWORD/ || /NOVA_API_KEY/ {print $1}')
+                PASSWORD=$(python -c "import keyring;print keyring.get_password('""$KEYNAME""', '""$2:$PASSKEY""')")
+                KEYRINGPARSEDFILE=$(sed -n -e "/\[$2\]/,/^$/p" $ADMINAPIFILE | sed -e '/^\[/d' -e 's/\ //g' -e "s/USE_KEYRING/$PASSWORD/g")
+                export $KEYRINGPARSEDFILE
+            else
+                export $PARSEDFILE
             fi
-
+        else
+            echo "The region was not specified or was invalid."
+            echo "Please try Again."
+            exit 1
+        fi
+    else
+        echo "Admin API config File is not found."
+        exit 1
+    fi
 fi
-
 export OS_USERNAME OS_REGION_NAME NOVA_RAX_AUTH OS_PASSWORD OS_AUTH_URL NOVA_VERSION NOVA_SERVICE_NAME OS_TENANT_NAME
-
 # If you use the go function it expects other functions too
 # This is a sanity check to make sure you have listed a function
 if [ ! -z $3 ];then
-	$NOVA $3 $4 $5 $6 $7 $8 $9;
-
+    $NOVA $3 $4 $5 $6 $7 $8 $9;
 # if no function was listed go will let you know and then show the help screen
 else
-echo ''
-echo "AND THEN??? You did not give any arguments, try again..."
-echo ''
-$NOVA help
-exit 0
-
+    echo ''
+    echo "AND THEN??? You did not give any arguments, try again..."
+    echo ''
+    $NOVA help
+    exit 0
 fi
 ;;
 
@@ -370,33 +326,28 @@ admin-set)
 if [ -z "$2" ];then
     echo "You need to specify an end point.\n"
     exit 1
-        else
-            CHECKFOREP=$(grep "\[$2\]" $ADMINAPIFILE)
-            if [ -z "$CHECKFOREP" ];then
-                echo -e "\nI did not find the \"\033[1;35m$2\033[0m\" endpoint\nyou should setup your \"\033[1;35m$ADMINAPIFILE\033[0m\" file accordingly.\n"
-                exit 1
-            fi
-fi
-    if [ -z "$3" ];then
-        echo "You need to specify an environment variable.\n"
+else
+    CHECKFOREP=$(grep "\[$2\]" $ADMINAPIFILE)
+    if [ -z "$CHECKFOREP" ];then
+        echo -e "\nI did not find the \"\033[1;35m$2\033[0m\" endpoint\nyou should setup your \"\033[1;35m$ADMINAPIFILE\033[0m\" file accordingly.\n"
         exit 1
     fi
+fi
+
+if [ -z "$3" ];then
+    echo "You need to specify an environment variable.\n"
+    exit 1
+fi
 
 echo -e "\n\nYou are setting a password in keyring\n"
 echo -e "You have set an Environment for     :\t\033[1;32m$2\033[0m"
 echo -e "You have set a Password Variable of :\t\033[1;32m$3\033[0m"
-
 read -s -p "Enter Password  : " MYPASSWORD
-
 python -c "import keyring;keyring.set_password('""kenova""','""$2:$3""','""$MYPASSWORD""')"
-
 echo -e "\n\nWe have setup a Key-Ring Password for your Environment \"\033[1;32m$2:$3\033[0m\"\n"
-
 ;;
-
 *)
 HELPINFORMATION
 exit 1
 ;;
-
 esac
