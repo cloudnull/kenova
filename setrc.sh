229bd05176384ff688f1ce0cdffe96eca6978c33
cloudnull <kevin.carter@rackspace.com>
2013-06-23 12:43:28 -0500

    added the rc setter for a local user

---
+++ setrc.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+# ==============================================================================
+# Copyright [2013] [Kevin Carter]
+# License Information :
+# This software has no warranty, it is provided 'as is'. It is your
+# responsibility to validate the behavior of the routines and its accuracy using
+# the code provided. Consult the GNU General Public license for further details
+# (see GNU General Public License).
+# http://www.gnu.org/licenses/gpl.html
+# ==============================================================================
+
+function setrc(){
+    if [[ ! $(grep "kenova" $RCFILE) ]];then
+        cat << EOF >> $RCFILE
+
+if [ -f $HOME/.kenova ];
+    source ~/.kenova
+fi
+
+EOF
+    else
+        echo -e "Looks like kenova is already in your RC file, so I did not re-add it"
+    fi
+}
+
+# Installer for the kenova command and control script
+echo -e "\nInstalling the kenova script\n"
+cp -v $WHEREAMI/kenova $HOME/.kenova >> $NOVALOG
+
+if [ -f "$HOME/.bashrc" ];then
+    RCFILE="$HOME/.bashrc"
+    setrc
+elif [ -f "$HOME/.profile" ];then
+    RCFILE="$HOME/.profile"
+    setrc
+elif [ -f "$HOME/.bash_profile" ];then
+    RCFILE="$HOME/.bash_profile"
+    setrc
+else
+    echo 'I could not file the RC file for setting Kenova, thus you have to do it yourself for what ever file you are using.'
+fi
