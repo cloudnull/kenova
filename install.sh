7aef0dd37ded5b520174fd81be8cd90505989f14
cloudnull <kevin.carter@rackspace.com>
2013-06-27 12:38:59 -0500

    updated for more of the chanage recommended by William Kelly

--- install.sh
+++ install.sh
@@ -9,11 +9,17 @@
 # http://www.gnu.org/licenses/gpl.html
 # ==============================================================================
 
-#set -u
-#set -e
+# Note that after running this script you will need to set source the kenova
+# file in your bash rc this can be done for you, if you run setrc.sh.
+# ==============================================================================
 
-# Temp Directory, This is Done like this because of OSX
-TMPDIR=$(mktemp -d XXXXXXXXXXX)
+set -u
+set -e
+
+FORCE=${1:-no}
+
+# Temp Log File, This is Done like this because of OSX
+NOVALOG="$(mktemp tmp.XXXXXXXXXXX).Kenova.Installation.log"
 
 # Installer Variables for NOVA
 NOVAVERSIONGIT="git://github.com/openstack/python-novaclient.git"
@@ -22,9 +28,6 @@ LNOVAVERSIONGIT="https://github.com/cloudnull/python-lnovaclient.git"
 SUPERNOVAGIT="https://github.com/major/supernova.git"
 KEYRINGNAME="keyring"
 
-# File Log
-NOVALOG="$TMPDIR/Nova.Installation.log"
-echo $NOVALOG
 # Test for setuptools
 if ! python -c 'import setuptools' &>/dev/null; then
     echo "Setup Tools was not found on your system" 1>&2
@@ -32,62 +35,51 @@ if ! python -c 'import setuptools' &>/dev/null; then
 fi
 
 # Test for requirements
-for req in python git pip; do
-    if ! which ${req};then
+for dep in python git pip; do
+    if [[ ! $(which ${dep}) ]];then
         echo "This program requires ${dep}, but ${dep} not found in PATH" 1>&2
         exit 1
     fi
 done
 
+# Root user check for install
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as ROOT. You have attempted to run this as $USER use sudo $@ or change to root." 1>&2
+   exit 1
+fi
+
 # Commands
-EZINST="$(which pip) install"
+EZINST="$(which pip) install --upgrade"
 
 # Information before Installation
 echo -e "
-You should know that if you install the nova environment using this script there
-will be several actions done.
+You should know that if you install the nova environment using this script global actions will be performed.
 
-1 - The installer will clean up known bad python modules that conflict with the
-    nova and lnova scripts.
-2 - If you have any parts of nova or lnova preinstalled, it will be removed.
-3 - Using Git, it will install all of the needed scripts for nova and lnova to
+1 - Using Git, it will install all of the needed parts of nova and lnova to
     work specifically with the Rackspace Openstack and Legacy Environments.
-"
-read -p "Type [ YES ] To continue. Otherwise press the sciprt will quit. : " CONFIRM
-if [[ ! "${CONFIRM}" == [yY][eE][sS] ]];then
-    echo "Exiting."
-    exit 1
-fi
 
-# Root user check for install
-if [ "$(id -u)" != "0" ]; then
-   echo -e "This script must be run as ROOT. You have attempted to run this as $( whoami ) use sudo $@ or change to root."
-   exit 1
+"
+if [[ ! "$FORCE" == "-f" ]];then
+    read -p "Type [ YES ] To continue. Otherwise press the script will quit. : " CONFIRM
+    if [[ ! "${CONFIRM}" == [yY][eE][sS] ]];then
+        echo "Exiting." 1>&2
+        exit 1
+    fi
 fi
 
 if [[ -f "$NOVALOG" ]]; then
     mv "$NOVALOG" "$NOVALOG.old.$(date +%y%m%d%H%M).log"
 fi
 
-for module in prettytable keyring novaclient rackspace_auth_openstack supernova; do
-    for locs in $(python -c "import site; print ' '.join(site.getsitepackages())"); do
-        find "${locs}" -type d -name "*${module}*" -exec rm -rfv {} \; >> $NOVALOG
-    done
-done
-
-echo -e "\nI am Installing the nova clients."
+echo "[+] I am Installing the nova clients." >> ${NOVALOG}
 ${EZINST} git+${NOVAVERSIONGIT} >> ${NOVALOG}
 ${EZINST} git+${LNOVAVERSIONGIT} >> ${NOVALOG}
-${EZINST} git+${SUPERNOVAGIT} >> ${NOVALOG}
-${EZINST} ${KEYRINGNAME} >> ${NOVALOG}
 ${EZINST} git+${RAXNOVAVERSIONGIT} >> ${NOVALOG}
 
-echo -e "
-The Log for the installation has been written to : $NOVALOG, all temp files are
-also in $TMPDIR. Now add the source to your environment RC file with command
+echo "[+] Installing the supernova and keyring for shits and giggles..." >> ${NOVALOG}
+${EZINST} ${KEYRINGNAME} >> ${NOVALOG}
+${EZINST} git+${SUPERNOVAGIT} >> ${NOVALOG}
 
-setrc.sh
-"
+echo "[+] The Log for the installation has been written to \"$NOVALOG\"."
 
 exit 0
-
